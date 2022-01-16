e37ae566eae726866317669461415242dd4db8fc
ljz <ljz@jiuqi.com.cn>
2022-01-16 20:28:42 +0800

    types

--- src/types.rs
+++ src/types.rs
@@ -1,11 +1,80 @@
-use std::fmt::Error;
-
-use windows::Win32::System::Com::VARIANT;
+use std::fmt::Display;
 
 #[derive(Debug)]
-pub enum Value<'a> {
+pub enum Value {
     NULL,
     INT(i32),
     LONG(i64),
-    STR(&'a str)
+    STR(String)
+}
+
+impl Value {
+    pub fn is_null(&self) -> bool {
+        if let Value::NULL = self {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn as_int(&self) -> i32 {
+        match self {
+            Value::NULL => 0,
+            Value::INT(value) => *value,
+            Value::LONG(value) => *value as i32,
+            Value::STR(value) => value.parse::<i32>().unwrap_or(0),
+        }
+    }
+
+    pub fn as_long(&self) -> i64 {
+        match self {
+            Value::NULL => 0,
+            Value::INT(value) => *value as i64,
+            Value::LONG(value) => *value,
+            Value::STR(value) => value.parse::<i64>().unwrap_or(0),
+        }
+    }
+
+    pub fn as_string(&self) -> Option<String>{
+        match self {
+            Value::NULL => Option::None,
+            Value::INT(value) => Option::Some(format!("{}", value)),
+            Value::LONG(value) => Option::Some(format!("{}", value)),
+            Value::STR(value) => Option::Some(value.clone()),
+        }
+    }
+}
+
+impl From<i16> for Value {
+    fn from(value: i16) -> Self {
+        Value::INT(value as i32)
+    }
+}
+impl From<i32> for Value {
+    fn from(value: i32) -> Self {
+        Value::INT(value)
+    }
+}
+
+impl From<i64> for Value {
+    fn from(value: i64) -> Self {
+        Value::LONG(value)
+    }
+}
+
+impl From<&str> for Value {
+    fn from(value: &str) -> Self {
+        Value::STR(String::from(value))
+    }
+}
+
+impl Display for Value {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Value::NULL => write!(f, "null"),
+            Value::INT(value) => write!(f, "{}", value),
+            Value::LONG(value) => write!(f, "{}", value),
+            Value::STR(value) => write!(f, "{}", value),
+        }
+    }
 }
