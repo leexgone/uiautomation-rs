e36cb5e5693caee57d566435d850f42e38475c50
ljz <ljz@jiuqi.com.cn>
2022-01-20 15:49:16 +0800

    去掉空值

--- src/types.rs
+++ src/types.rs
@@ -2,24 +2,14 @@ use std::fmt::Display;
 
 #[derive(Debug)]
 pub enum Value {
-    NULL,
     INT(i32),
     LONG(i64),
     STR(String)
 }
 
 impl Value {
-    pub fn is_null(&self) -> bool {
-        if let Value::NULL = self {
-            true
-        } else {
-            false
-        }
-    }
-
     pub fn as_int(&self) -> i32 {
         match self {
-            Value::NULL => 0,
             Value::INT(value) => *value,
             Value::LONG(value) => *value as i32,
             Value::STR(value) => value.parse::<i32>().unwrap_or(0),
@@ -28,7 +18,6 @@ impl Value {
 
     pub fn as_long(&self) -> i64 {
         match self {
-            Value::NULL => 0,
             Value::INT(value) => *value as i64,
             Value::LONG(value) => *value,
             Value::STR(value) => value.parse::<i64>().unwrap_or(0),
@@ -37,7 +26,6 @@ impl Value {
 
     pub fn as_string(&self) -> Option<String>{
         match self {
-            Value::NULL => Option::None,
             Value::INT(value) => Option::Some(format!("{}", value)),
             Value::LONG(value) => Option::Some(format!("{}", value)),
             Value::STR(value) => Option::Some(value.clone()),
@@ -71,7 +59,6 @@ impl From<&str> for Value {
 impl Display for Value {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Value::NULL => write!(f, "null"),
             Value::INT(value) => write!(f, "{}", value),
             Value::LONG(value) => write!(f, "{}", value),
             Value::STR(value) => write!(f, "{}", value),
