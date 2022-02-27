b5bcef145ba38fedea49dfe8378a549eff0d8b88
ljz <ljz@jiuqi.com.cn>
2022-02-27 22:10:29 +0800

    UIAnnotationPattern

--- src/patterns.rs
+++ src/patterns.rs
@@ -1,8 +1,11 @@
+use windows::Win32::UI::Accessibility::IUIAutomationAnnotationPattern;
 use windows::Win32::UI::Accessibility::IUIAutomationInvokePattern;
+use windows::Win32::UI::Accessibility::UIA_AnnotationPatternId;
 use windows::Win32::UI::Accessibility::UIA_InvokePatternId;
 use windows::core::IUnknown;
 use windows::core::Interface;
 
+use crate::core::UIElement;
 use crate::errors::Error;
 use crate::errors::Result;
 
@@ -34,14 +37,6 @@ impl UIPattern for UIInvokePattern {
     }
 }
 
-impl From<IUIAutomationInvokePattern> for UIInvokePattern {
-    fn from(pattern: IUIAutomationInvokePattern) -> Self {
-        Self {
-            pattern
-        }
-    }
-}
-
 impl TryFrom<IUnknown> for UIInvokePattern {
     type Error = Error;
 
@@ -53,6 +48,14 @@ impl TryFrom<IUnknown> for UIInvokePattern {
     }
 }
 
+impl From<IUIAutomationInvokePattern> for UIInvokePattern {
+    fn from(pattern: IUIAutomationInvokePattern) -> Self {
+        Self {
+            pattern
+        }
+    }
+}
+
 impl Into<IUIAutomationInvokePattern> for UIInvokePattern {
     fn into(self) -> IUIAutomationInvokePattern {
         self.pattern
@@ -61,6 +64,88 @@ impl Into<IUIAutomationInvokePattern> for UIInvokePattern {
 
 impl AsRef<IUIAutomationInvokePattern> for UIInvokePattern {
     fn as_ref(&self) -> &IUIAutomationInvokePattern {
+        &self.pattern
+    }
+}
+
+pub struct UIAnnotationPattern {
+    pattern: IUIAutomationAnnotationPattern
+}
+
+impl UIAnnotationPattern {
+    pub fn get_type_id(&self) -> Result<i32> {
+        let id = unsafe {
+            self.pattern.CurrentAnnotationTypeId()?
+        };
+        Ok(id)
+    }
+
+    pub fn get_type_nane(&self) -> Result<String> {
+        let name = unsafe {
+            self.pattern.CurrentAnnotationTypeName()?
+        };
+        Ok(name.to_string())
+    }
+
+    pub fn get_author(&self) -> Result<String> {
+        let author = unsafe {
+            self.pattern.CurrentAuthor()?
+        };
+        Ok(author.to_string())
+    }
+
+    pub fn get_datetime(&self) -> Result<String> {
+        let datetime = unsafe {
+            self.pattern.CurrentDateTime()?
+        };
+        Ok(datetime.to_string())
+    }
+
+    pub fn get_target(&self) -> Result<UIElement> {
+        let target = unsafe {
+            self.pattern.CurrentTarget()?
+        };
+        Ok(target.into())
+    }
+}
+
+impl UIPattern for UIAnnotationPattern {
+    fn pattern_id() -> i32 {
+        UIA_AnnotationPatternId
+    }
+
+    fn new(pattern: IUnknown) -> Result<Self> {
+        UIAnnotationPattern::try_from(pattern)
+    }
+}
+
+impl TryFrom<IUnknown> for UIAnnotationPattern {
+    type Error = Error;
+
+    fn try_from(value: IUnknown) -> core::result::Result<Self, Self::Error> {
+        let pattern: IUIAutomationAnnotationPattern = value.cast()?;
+        Ok(Self {
+            pattern
+        })
+    }
+}
+
+impl From<IUIAutomationAnnotationPattern> for UIAnnotationPattern {
+    fn from(pattern: IUIAutomationAnnotationPattern) -> Self {
+        Self {
+            pattern
+        }
+    }
+}
+
+impl Into<IUIAutomationAnnotationPattern> for UIAnnotationPattern {
+    fn into(self) -> IUIAutomationAnnotationPattern {
+        self.pattern
+    }
+}
+
+impl AsRef<IUIAutomationAnnotationPattern> for UIAnnotationPattern {
+    fn as_ref(&self) -> &IUIAutomationAnnotationPattern {
         &self.pattern
     }
 }
