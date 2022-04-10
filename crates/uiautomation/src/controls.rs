dc00d5fe086c55a09a407003777871277a22c258
ljz <ljz@jiuqi.com.cn>
2022-04-10 11:37:41 +0800

    controls

--- crates/uiautomation/src/controls.rs
+++ crates/uiautomation/src/controls.rs
@@ -1,6 +1,7 @@
 use windows::Win32::UI::Accessibility::IUIAutomationElement;
 use windows::Win32::UI::Accessibility::UIA_ButtonControlTypeId;
 use windows::Win32::UI::Accessibility::UIA_InvokePatternId;
+use windows::Win32::UI::Accessibility::UIA_ListControlTypeId;
 use windows::Win32::UI::Accessibility::UIA_ListItemControlTypeId;
 use windows::Win32::UI::Accessibility::UIA_SelectionItemPatternId;
 
@@ -9,8 +10,11 @@ use crate::Result;
 use crate::UIElement;
 use crate::errors::ERR_TYPE;
 use crate::patterns::UIInvokePattern;
+use crate::patterns::UIItemContainerPattern;
+use crate::patterns::UIMultipleViewPattern;
 use crate::patterns::UIScrollItemPattern;
 use crate::patterns::UISelectionItemPattern;
+use crate::variants::Variant;
 
 /// Wrapper an button element as a control.
 pub struct ButtonControl {
@@ -20,7 +24,7 @@ pub struct ButtonControl {
 impl ButtonControl {
     /// Perform a click event on this control.
     pub fn click(&self) -> Result<()> {
-        let pattern: UIInvokePattern = self.control.get_pattern()?;
+        let pattern: UIInvokePattern = self.as_ref().get_pattern()?;
         pattern.invoke()
     }
 }
@@ -28,10 +32,10 @@ impl ButtonControl {
 impl TryFrom<UIElement> for ButtonControl {
     type Error = Error;
 
-    fn try_from(value: UIElement) -> Result<Self> {
-        if value.get_control_type()? == UIA_ButtonControlTypeId {
+    fn try_from(control: UIElement) -> Result<Self> {
+        if control.get_control_type()? == UIA_ButtonControlTypeId {
             Ok(Self {
-                control: value
+                control
             })
         } else {
             Err(Error::new(ERR_TYPE, "Error Control Type"))
@@ -54,6 +58,68 @@ impl AsRef<UIElement> for ButtonControl {
 /// Wrapper a list element as a control. The control type of the element must be `UIA_ListControlTypeId`.
 pub struct ListControl {
     control: UIElement
+}
+
+impl ListControl {
+    /// Find contained item by property value.
+    /// 
+    /// Search `UIElement` from the `start_ater` item, return the first item which property is specified value.
+    pub fn find_item_by_property(&self, start_after: UIElement, property_id: i32, value: Variant) -> Result<UIElement> {
+        let pattern: UIItemContainerPattern = self.control.get_pattern()?;
+        pattern.find_item_by_property(start_after, property_id, value)
+    }
+
+    /// Get supported view ids.
+    pub fn get_supported_views(&self) -> Result<Vec<i32>> {
+        let pattern: UIMultipleViewPattern = self.control.get_pattern()?;
+        pattern.get_supported_views()
+    }
+
+    /// Return the view name.
+    /// 
+    /// The `view` parameter is the id of the view. You can get all view ids by `get_supported_views()` function.
+    pub fn get_view_name(&self, view: i32) -> Result<String> {
+        let pattern: UIMultipleViewPattern = self.control.get_pattern()?;
+        pattern.get_view_name(view)
+    }
+
+    /// Get the current view id.
+    pub fn get_current_view(&self) -> Result<i32> {
+        let pattern: UIMultipleViewPattern = self.control.get_pattern()?;
+        pattern.get_current_view()
+    }
+
+    /// Set the current view by id.
+    pub fn set_current_view(&self, view: i32) -> Result<()> {
+        let pattern: UIMultipleViewPattern = self.control.get_pattern()?;
+        pattern.set_current_view(view)
+    }
+}
+
+impl TryFrom<UIElement> for ListControl {
+    type Error = Error;
+
+    fn try_from(control: UIElement) -> Result<Self> {
+        if control.get_control_type()? == UIA_ListControlTypeId {
+            Ok(Self {
+                control
+            })
+        } else {
+            Err(Error::new(ERR_TYPE, "Error Control Type"))
+        }
+    }
+}
+
+impl Into<UIElement> for ListControl {
+    fn into(self) -> UIElement {
+        self.control
+    }
+}
+
+impl AsRef<UIElement> for ListControl {
+    fn as_ref(&self) -> &UIElement {
+        &self.control
+    }
 }
 
 /// Wrapper a listitem element as a control. The control type of the element must be `UIA_ListItemControlTypeId`.
@@ -120,10 +186,10 @@ impl ListItemControl {
 impl TryFrom<UIElement> for ListItemControl {
     type Error = Error;
 
-    fn try_from(value: UIElement) -> Result<Self> {
-        if value.get_control_type()? == UIA_ListItemControlTypeId {
+    fn try_from(control: UIElement) -> Result<Self> {
+        if control.get_control_type()? == UIA_ListItemControlTypeId {
             Ok(Self {
-                control: value
+                control
             })
         } else {
             Err(Error::new(ERR_TYPE, "Error Control Type"))
