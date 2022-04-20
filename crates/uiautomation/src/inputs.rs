e6c1e9586bd2c7cac5eabe24a2e3c564b9e97210
ljz <ljz@jiuqi.com.cn>
2022-04-20 21:46:20 +0800

    input test

--- crates/uiautomation/src/inputs.rs
+++ crates/uiautomation/src/inputs.rs
@@ -1,10 +1,71 @@
 #[cfg(test)]
 mod tests {
+    use std::mem;
+
     use windows::Win32::UI::Input::KeyboardAndMouse::INPUT;
+    use windows::Win32::UI::Input::KeyboardAndMouse::INPUT_0;
+    use windows::Win32::UI::Input::KeyboardAndMouse::INPUT_KEYBOARD;
+    use windows::Win32::UI::Input::KeyboardAndMouse::KEYBDINPUT;
+    use windows::Win32::UI::Input::KeyboardAndMouse::KEYBD_EVENT_FLAGS;
+    use windows::Win32::UI::Input::KeyboardAndMouse::KEYEVENTF_KEYUP;
     use windows::Win32::UI::Input::KeyboardAndMouse::SendInput;
+    use windows::Win32::UI::Input::KeyboardAndMouse::VK_D;
+    use windows::Win32::UI::Input::KeyboardAndMouse::VK_LWIN;
 
     #[test]
     fn show_desktop() {
-        let mut inputs: [INPUT; 4] = [INPUT::default(); 4];
+        let inputs: [INPUT; 4] = [ INPUT {
+            r#type: INPUT_KEYBOARD,
+            Anonymous: INPUT_0 { 
+                ki: KEYBDINPUT {
+                    wVk: VK_LWIN,
+                    wScan: 0,
+                    dwFlags: KEYBD_EVENT_FLAGS::default(),
+                    time: 0,
+                    dwExtraInfo: 0
+                } 
+            }
+        }, INPUT {
+            r#type: INPUT_KEYBOARD,
+            Anonymous: INPUT_0 {
+                ki: KEYBDINPUT { 
+                    wVk: VK_D, 
+                    wScan: 0, 
+                    dwFlags: KEYBD_EVENT_FLAGS::default(), 
+                    time: 0, 
+                    dwExtraInfo: 0 
+                }
+            }
+        }, INPUT {
+            r#type: INPUT_KEYBOARD,
+            Anonymous: INPUT_0 {
+                ki: KEYBDINPUT { 
+                    wVk: VK_D, 
+                    wScan: 0, 
+                    dwFlags: KEYEVENTF_KEYUP, 
+                    time: 0, 
+                    dwExtraInfo: 0 
+                }
+            }
+        }, INPUT {
+            r#type: INPUT_KEYBOARD,
+            Anonymous: INPUT_0 {
+                ki: KEYBDINPUT { 
+                    wVk: VK_LWIN, 
+                    wScan: 0, 
+                    dwFlags: KEYEVENTF_KEYUP, 
+                    time: 0, 
+                    dwExtraInfo: 0 
+                }
+            }
+        }];
+        let size = mem::size_of_val(&inputs);
+        println!("{}", size);
+
+        let sent = unsafe {
+            SendInput(&inputs, size as _)
+        };
+        
+        assert_eq!(sent as usize, inputs.len());
     }
 }
