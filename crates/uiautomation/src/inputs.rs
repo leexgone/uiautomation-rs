57ccb073e9457fdf3b5aded9af12aa340eacf73b
ljz <ljz@jiuqi.com.cn>
2022-04-20 18:06:03 +0800

    inputs

--- crates/uiautomation/src/inputs.rs
+++ crates/uiautomation/src/inputs.rs
@@ -1,7 +1,10 @@
 #[cfg(test)]
 mod tests {
+    use windows::Win32::UI::Input::KeyboardAndMouse::INPUT;
+    use windows::Win32::UI::Input::KeyboardAndMouse::SendInput;
+
     #[test]
     fn show_desktop() {
-        
+        let mut inputs: [INPUT; 4] = [INPUT::default(); 4];
     }
 }
