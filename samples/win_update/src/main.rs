f91c1662968518532031774e922191b09a606a65
ljz <ljz@jiuqi.com.cn>
2022-08-03 15:49:27 +0800

    update win-update

--- samples/win_update/src/main.rs
+++ samples/win_update/src/main.rs
@@ -4,8 +4,10 @@ use uiautomation::actions::SelectionItem;
 use uiautomation::actions::Toggle;
 use uiautomation::controls::ButtonControl;
 use uiautomation::controls::ListItemControl;
+use uiautomation::controls::WindowControl;
 use windows::Win32::UI::Accessibility::UIA_ButtonControlTypeId;
 use windows::Win32::UI::Accessibility::UIA_ListItemControlTypeId;
+use windows::Win32::UI::Accessibility::UIA_ToolBarControlTypeId;
 
 fn main() {
     let automation = UIAutomation::new().unwrap();
@@ -27,7 +29,11 @@ fn main() {
 
     let matcher = automation.create_matcher().match_name("设置").classname("ApplicationFrameWindow");
     let settings = matcher.find_first().unwrap();
+    
+    let window: WindowControl = settings.clone().try_into().unwrap();
+    window.set_foregrand().unwrap();
     settings.set_focus().unwrap();
+
     let matcher = automation.create_matcher().from(settings.clone()).match_name("Windows 更新").control_type(UIA_ListItemControlTypeId);
     let update = matcher.find_first().unwrap();
     // println!("{}", update.get_control_type().unwrap());
@@ -39,5 +45,15 @@ fn main() {
     if update.is_enabled().unwrap() {
         let button: ButtonControl = update.try_into().unwrap();
         button.invoke().unwrap();
+    }
+
+    let matcher = automation.create_matcher().name("运行中的应用程序").control_type(UIA_ToolBarControlTypeId);
+    if let Ok(taskbar) = matcher.find_first() {
+        let matcher = automation.create_matcher().from(taskbar).contains_name("设置").control_type(UIA_ButtonControlTypeId);
+        if let Ok(settings_button) = matcher.find_first() {
+            settings_button.click().unwrap();
+            // let settings_button: ButtonControl = settings_button.try_into().unwrap();
+            // settings_button.invoke().unwrap();
+        }
     }
 }
