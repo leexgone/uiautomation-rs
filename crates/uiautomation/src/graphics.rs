ad8707f1c53fe2d0e31d632f6b976b2d30849d64
ljz <ljz@jiuqi.com.cn>
2022-09-29 20:47:09 +0800

    canvas dev

--- crates/uiautomation/src/graphics.rs
+++ crates/uiautomation/src/graphics.rs
@@ -12,10 +12,16 @@ use crate::Result;
 use crate::errors::ERR_INVALID_OBJECT;
 use crate::types::Handle;
 
+#[derive(Debug)]
+enum HDCOrigin {
+    Created(CreatedHDC),
+    GotFrom(Handle)    
+}
+
 /// Canvas is a wrapper for `HDC` object which can used as a gdi canvas.
 #[derive(Debug)]
 pub struct Canvas{
-    window: Option<Handle>,
+    origin: HDCOrigin,
     hdc: HDC
 }
 
@@ -27,9 +33,9 @@ impl TryFrom<Handle> for Canvas {
             GetDC(wnd_handle)    
         };
 
-        if hdc.is_invalid() {
+        if !hdc.is_invalid() {
             Ok(Canvas {
-                window: Some(wnd_handle),
+                origin: HDCOrigin::GotFrom(wnd_handle),
                 hdc: hdc
             })
         } else {
@@ -39,25 +45,40 @@ impl TryFrom<Handle> for Canvas {
 }
 
 impl Canvas {
-    // pub fn create() -> Result<Canvas> {
-    //     CreateCompatibleDC(hdc)
-    // }
-
+    /// Determines whether the canvas is valid.
     pub fn is_valid(&self) -> bool {
-        self.hdc.is_invalid()
+        !self.hdc.is_invalid()
+    }
+
+    /// Creates a memory canvas compatible with the current canvas.
+    pub fn create_compatible_canvas(&self) -> Result<Canvas> {
+        let hdc  = unsafe {
+            CreateCompatibleDC(self.hdc)
+        };
+
+        if !hdc.is_invalid() {
+            Ok(Canvas { 
+                origin: HDCOrigin::Created(hdc), 
+                hdc: HDC(hdc.0) 
+            })
+        } else {
+            Err(Error::new(ERR_INVALID_OBJECT, "Invalid dc object"))
+        }
     }
 }
 
 impl Drop for Canvas {
     fn drop(&mut self) {
         if self.is_valid() {
-            match self.window {
-                Some(hwnd) => unsafe { ReleaseDC(hwnd, self.hdc); },
-                None => {
-                    let hdc = CreatedHDC(self.hdc.0);
-                    unsafe { DeleteDC(hdc) }; 
+            match self.origin {
+                HDCOrigin::Created(cdc) => unsafe {
+                    DeleteDC(cdc);
+                },
+                HDCOrigin::GotFrom(wnd) => unsafe {
+                    ReleaseDC(wnd, self.hdc);
                 }
             }
+            self.hdc = HDC::default();
         }
     }
 }
