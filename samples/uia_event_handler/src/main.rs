160f64a04167d199b72099f8791d35754f3152c0
Steven Lee <leexgone@163.com>
2025-05-27 09:16:15 +0800

    #107

--- samples/uia_event_handler/src/main.rs
+++ samples/uia_event_handler/src/main.rs
@@ -1,7 +1,9 @@
 use uiautomation::events::CustomFocusChangedEventHandler;
 use uiautomation::events::CustomPropertyChangedEventHandlerFn;
+use uiautomation::events::CustomStructureChangedEventHandler;
 use uiautomation::events::UIFocusChangedEventHandler;
 use uiautomation::events::UIPropertyChangedEventHandler;
+use uiautomation::events::UIStructureChangeEventHandler;
 use uiautomation::processes::Process;
 use uiautomation::types::UIProperty;
 use uiautomation::UIAutomation;
@@ -11,6 +13,15 @@ struct MyFocusChangedEventHandler{}
 impl CustomFocusChangedEventHandler for MyFocusChangedEventHandler {
     fn handle(&self, sender: &uiautomation::UIElement) -> uiautomation::Result<()> {
         println!("Focus changed: {}", sender);
+        Ok(())
+    }
+}
+
+struct MyStructureChangeEventHandler {}
+
+impl CustomStructureChangedEventHandler for MyStructureChangeEventHandler {
+    fn handle(&self, sender: &uiautomation::UIElement, change_type: uiautomation::types::StructureChangeType, _runtime_id: Option<&[i32]>) -> uiautomation::Result<()> {
+        println!("Structure changed: {}. Change type: {}.", sender, change_type);
         Ok(())
     }
 }
@@ -34,6 +45,11 @@ fn main() {
         let text_changed_handler = UIPropertyChangedEventHandler::from(text_changed_handler);
 
         automation.add_property_changed_event_handler(&notepad, uiautomation::types::TreeScope::Subtree, None, &text_changed_handler, &[UIProperty::ValueValue]).unwrap();
+
+        let structure_changed_handler = MyStructureChangeEventHandler {};
+        let structure_changed_handler = UIStructureChangeEventHandler::from(structure_changed_handler);
+
+        automation.add_structure_changed_event_handler(&notepad, uiautomation::types::TreeScope::Subtree, None, &structure_changed_handler).unwrap();
     }
 
     println!("waiting for notepad.exe...");
