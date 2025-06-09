da0f31c2d9d9d6fef3e62e7aa37532a59daecae0
Steven Lee <leexgone@163.com>
2025-06-09 18:40:15 +0800

    remove comments

--- crates/uiautomation/src/patterns.rs
+++ crates/uiautomation/src/patterns.rs
@@ -1553,51 +1553,6 @@ impl UITextEditPattern {
     }
 }
 
-// impl UIPattern for UITextEditPattern {
-//     const TYPE: UIPatternType = UIPatternType::TextEdit;
-// }
-
-// impl TryFrom<IUnknown> for UITextEditPattern {
-//     type Error = Error;
-
-//     fn try_from(value: IUnknown) -> Result<Self> {
-//         let pattern: IUIAutomationTextEditPattern = value.cast()?;
-//         let text = UITextPattern::try_from(value)?;
-//         Ok(Self {
-//             text,
-//             pattern
-//         })
-//     }
-// }
-
-// impl From<IUIAutomationTextEditPattern> for UITextEditPattern {
-//     fn from(pattern: IUIAutomationTextEditPattern) -> Self {
-//         let text: IUnknown = pattern.cast().unwrap();
-//         Self {
-//             text: text.try_into().unwrap(),
-//             pattern
-//         }
-//     }
-// }
-
-// impl Into<IUIAutomationTextEditPattern> for UITextEditPattern {
-//     fn into(self) -> IUIAutomationTextEditPattern {
-//         self.pattern
-//     }
-// }
-
-// impl AsRef<IUIAutomationTextEditPattern> for UITextEditPattern {
-//     fn as_ref(&self) -> &IUIAutomationTextEditPattern {
-//         &self.pattern
-//     }
-// }
-
-// impl AsRef<UITextPattern> for UITextEditPattern {
-//     fn as_ref(&self) -> &UITextPattern {
-//         &self.text
-//     }
-// }
-
 /// A wrapper for `IUIAutomationTextRange`, `IUIAutomationTextRange2` and `IUIAutomationTextRange3`.
 #[derive(Debug, Clone)]
 pub struct UITextRange {
